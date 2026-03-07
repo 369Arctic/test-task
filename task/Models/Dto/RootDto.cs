45a665e93b6639688c704e27f35ee1e51c2f9769
Dmitriy Sannikov <omeom@mail.ru>
2026-03-07 16:50:40 +0400

    feat: добавлены DTO для корректной десериализации

---
+++ task/Models/Dto/RootDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task.Models.Dto
+{
+    internal class RootDto
+    {
+        public List<CityDto>? City { get; set; }
+    }
+}
---
+++ task/Models/Dto/TerminalDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task.Models.Dto
+{
+    internal class TerminalDto
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public string? Latitude { get; set; }
+        public string? Longitude { get; set; }
+        public List<PhoneDto>? Phones { get; set; }
+    }
+}
