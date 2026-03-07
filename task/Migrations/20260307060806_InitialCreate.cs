b1e9a5dbe827f032012906e950101bae997c1b09
Dmitriy Sannikov <omeom@mail.ru>
2026-03-07 10:15:29 +0400

    feat: добавлены конфигурации для БД, миграция

---
+++ task/Migrations/20260307060806_InitialCreate.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace task.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Offices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Code = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    CityCode = table.Column<int>(type: "integer", nullable: false),
+                    Uuid = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    Type = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: true),
+                    CountryCode = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    latitude = table.Column<double>(type: "double precision", nullable: false),
+                    longitude = table.Column<double>(type: "double precision", nullable: false),
+                    AddressRegion = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    AddressCity = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    AddressStreet = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    AddressHouseNumber = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    AddressApartment = table.Column<int>(type: "integer", nullable: true),
+                    WorkTime = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Offices", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Phone",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    OfficeId = table.Column<int>(type: "integer", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Additional = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Phone", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Phone_Offices_OfficeId",
+                        column: x => x.OfficeId,
+                        principalTable: "Offices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Offices_CityCode",
+                table: "Offices",
+                column: "CityCode");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Offices_Code",
+                table: "Offices",
+                column: "Code");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Phone_OfficeId",
+                table: "Phone",
+                column: "OfficeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Phone");
+
+            migrationBuilder.DropTable(
+                name: "Offices");
+        }
+    }
+}
