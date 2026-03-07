5ffa298ae66e959d1f723368cfca1e3cf9a54aa3
Dmitriy Sannikov <omeom@mail.ru>
2026-03-07 16:11:00 +0400

    fix: реализован сервис импорта терминалов

---
+++ task/Services/Interfaces/ITerminalImportService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task.Services.Interfaces
+{
+    public interface ITerminalImportService
+    {
+        Task ImportAsync(string filePath, CancellationToken cancellationToken);
+    }
+}
